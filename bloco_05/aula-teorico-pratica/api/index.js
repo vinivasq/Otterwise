9467273e0b999493ca3a4312f159b696ddccd401
Vinícius Vasques <vinivasq@hotmail.com>
2022-12-07 23:21:59 -0300

    requisições http

--- bloco_05/aula-teorico-pratica/api/index.js
+++ bloco_05/aula-teorico-pratica/api/index.js
@@ -1,10 +1,63 @@
 import axios from "axios";
 
 const getPosts = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
-    console.log(response.data);
+    try{
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts/12");
+        
+        console.log(response.data);
+        return response
 
-    return response
+    }catch(error){
+        console.log(error.message);
+    }
+}
+// getPosts()
+
+const data = {
+    userId: 10,
+    title: "Meu titulo",
 }
 
-getPosts()
+const setPost = async (data) => {
+    try{
+        const response = await axios.post("https://jsonplaceholder.typicode.com/posts", data)
+        
+        console.log(response.data)
+    }catch(error){
+        console.log(error.response.status);
+    }
+}
+// setPost(data)
+
+const putPost = async (data, value) => {
+    try{
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${value}`, data)
+
+        console.log(response.data)
+    } catch(error) {
+        console.log(error.response.status)
+    }
+}
+// putPost(data,1)
+
+const patchPost = async (data, value) => {
+    try{
+        const response = await axios.patch(`https://jsonplaceholder.typicode.com/posts/${value}`, data)
+
+        console.log(response.data)
+    } catch (error) {
+        console.log(error.response.status)
+    }
+}
+// patchPost(data, 1)
+
+const deletePost = async (value) => {
+    try {
+        const response = await axios.delete(`https://jsonplaceholder.typicode.com/posts/${value}`)
+
+        console.log(response.data);
+    } catch(error) {
+        console.log(error.response.status);
+    }
+}
+// deletePost(1)
