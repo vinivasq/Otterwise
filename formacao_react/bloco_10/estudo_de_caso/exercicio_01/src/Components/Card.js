e6865a7e4dad78cd2d8435260880b1ecb209b779
Vinícius Vasques <vinivasq@hotmail.com>
2023-02-21 11:54:02 -0300

    utilizando useState no exercicio

--- formacao_react/bloco_10/estudo_de_caso/exercicio_01/src/Components/Card.js
+++ formacao_react/bloco_10/estudo_de_caso/exercicio_01/src/Components/Card.js
@@ -1,11 +1,22 @@
 import "./Card.css";
+import { useState } from "react";
 
 const Card = (props) => {
   const { image, title, description } = props;
+  const [display, setDisplay] = useState("none");
+
   return (
     <div className="card">
       <img className="card__image" src={image} alt="Imagem da Skill" />
-      <h2 className="card__title">{title}</h2>
+      <h2
+        onClick={(e) => {
+          setDisplay();
+          e.target.parentElement.style.display = display;
+        }}
+        className="card__title"
+      >
+        {title}
+      </h2>
       <p className="card__description">{description}</p>
     </div>
   );
