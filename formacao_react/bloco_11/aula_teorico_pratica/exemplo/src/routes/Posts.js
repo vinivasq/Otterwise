de2fc689ae6c11ee9cb27b8e894525d566e777c2
vinivasq <vinivasq@hotmail.com>
2023-02-27 14:43:34 -0300

    url e search parameters

--- formacao_react/bloco_11/aula_teorico_pratica/exemplo/src/routes/Posts.js
+++ formacao_react/bloco_11/aula_teorico_pratica/exemplo/src/routes/Posts.js
@@ -1,19 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { getPost, listAllPosts } from "../services/posts";
 import { useEffect, useState } from "react";
 
 const Posts = () => {
   const [data, setData] = useState(null);
   const { id } = useParams();
-  console.log(id);
+  const [searchParams] = useSearchParams();
+  const userId = searchParams.get("userId");
+
+  console.log(userId);
 
   useEffect(() => {
     const request = async () => {
-      const response = id ? await getPost(id) : await listAllPosts();
+      const response = id ? await getPost(id) : await listAllPosts({ userId });
+      console.log(response.data);
       setData(response.data);
     };
     request();
-  }, [id]);
+  }, []);
 
   if (!data) return <p>Não existem posts a serem exibidos</p>;
   if (id) return <p>{data.title}</p>;
