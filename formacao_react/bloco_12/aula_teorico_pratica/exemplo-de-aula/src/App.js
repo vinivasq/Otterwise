eb338a2a234443f3a475275c8edd563341101802
vinivasq <vinivasq@hotmail.com>
2023-03-03 14:43:30 -0300

    react hook form e yup para formularios

--- formacao_react/bloco_12/aula_teorico_pratica/exemplo-de-aula/src/App.js
+++ formacao_react/bloco_12/aula_teorico_pratica/exemplo-de-aula/src/App.js
@@ -1,5 +1,45 @@
-function App() {
-  return <div></div>;
-}
+import { useForm } from "react-hook-form";
+import { login } from "./services/auth";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-export default App;
+export default function App() {
+  const schema = yup
+    .object({
+      email: yup.string().email().required("Email é obrigatório"),
+      password: yup
+        .string()
+        .min(5, "Minimo de 5 caracteres")
+        .required("Senha é obrigatória"),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = async (data) => {
+    try {
+      console.log(data);
+      const response = await login(data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="email" {...register("email")} />
+      {errors.email && <span>{errors.email.message}</span>}
+
+      <input type="password" {...register("password")} />
+      {errors.password && <span>{errors.password.message}</span>}
+
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
---
+++ formacao_react/bloco_12/aula_teorico_pratica/exemplo-de-aula/src/providers/auth.js
@@ -0,0 +1,26 @@
+import { login } from "../services/auth";
+
+export const authProvider = {
+  isAuthenticated: false,
+  signIn: async (data, callback) => {
+    try {
+      const response = await login(data);
+      const user = {
+        token: response.data.token,
+        name: response.data.user.name,
+      };
+
+      localStorage.setItem("user", JSON.stringify(user));
+      callback(user);
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  signOut: (callback) => {
+    localStorage.removeItem("user");
+    callback();
+  },
+};
