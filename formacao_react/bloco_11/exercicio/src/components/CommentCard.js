86137309b875ed9c911d2e11560ee889e68032c6
vinivasq <vinivasq@hotmail.com>
2023-03-07 13:52:00 -0300

    inserção do chakra provider e finalizada estilização do card

--- formacao_react/bloco_11/exercicio/src/components/CommentCard.js
+++ formacao_react/bloco_11/exercicio/src/components/CommentCard.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Card, CardHeader, CardBody, Text, Heading } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Text,
+  Heading,
+  Stack,
+  StackDivider,
+  Box,
+} from "@chakra-ui/react";
 
 const CommentCard = (props) => {
   const { title, description } = props;
@@ -10,15 +18,21 @@ const CommentCard = (props) => {
       padding="1rem"
       m={"auto"}
       mt="1rem"
-      backgroundColor="#f1f5f9"
-      borderRadius="5px"
-      boxShadow="1px 1px 5px black"
+      backgroundColor="#4338ca"
+      borderRadius="md"
+      boxShadow="md"
     >
-      <CardHeader>
-        <Heading size="md">{title}</Heading>
-      </CardHeader>
       <CardBody>
-        <Text>{description}</Text>
+        <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="sm" color="white">
+              {title}
+            </Heading>
+          </Box>
+          <Box>
+            <Text color="white">{description}</Text>
+          </Box>
+        </Stack>
       </CardBody>
     </Card>
   );
