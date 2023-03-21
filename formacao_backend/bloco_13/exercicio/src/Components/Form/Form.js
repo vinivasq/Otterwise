9432e6b70ddef043c4bed4d6475ec4f3efd81289
Vinícius Vasques <vinivasq@hotmail.com>
2023-03-21 10:31:08 -0300

    recuperação dos dados do formulario com hook form e conversão para Form Data

--- formacao_backend/bloco_13/exercicio/src/Components/Form/Form.js
+++ formacao_backend/bloco_13/exercicio/src/Components/Form/Form.js
@@ -1,7 +1,23 @@
 import styled from "styled-components";
 import Label from "./Label";
-import Input from "./Input";
 import Button from "../Button/Button";
+import { useForm } from "react-hook-form";
+
+const StyledInput = styled.input`
+  padding: 0.5rem;
+  border-radius: 5px;
+  width: 90%;
+  border: 1px solid black;
+
+  &:focus {
+    transition: all 400ms;
+    border: 1px solid blueviolet;
+    outline: none;
+  }
+  @media screen and (min-width: 992px) {
+    width: 350px;
+  }
+`;
 
 const StyledForm = styled.form`
   display: flex;
@@ -12,30 +28,37 @@ const StyledForm = styled.form`
 `;
 
 const Form = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    const dataEntries = Object.entries(data);
+
+    dataEntries.forEach((data) => {
+      formData.append(data[0], data[1]);
+    });
+
+    console.log(formData.get("image"));
+  };
+
   return (
-    <StyledForm
-      onSubmit={(e) => {
-        e.preventDefault();
-        console.log(e);
-      }}
-    >
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
       <Label>
         Selecione uma imagem:
-        <input type="file" />
+        <input type="file" {...register("image")} />
       </Label>
       <Label>
         Título:
-        <Input />
+        <StyledInput {...register("title")} />
       </Label>
       <Label>
         Conteúdo
-        <Input />
+        <StyledInput {...register("content")} />
       </Label>
       <Label>
         Usuário
-        <Input />
+        <StyledInput {...register("authorId")} />
       </Label>
-      <Button />{" "}
+      <Button />
     </StyledForm>
   );
 };
