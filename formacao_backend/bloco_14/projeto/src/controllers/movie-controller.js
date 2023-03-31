1967f9966f2d64b9880e9ad598e278c6afa8602d
Vinícius Vasques <vinivasq@hotmail.com>
2023-03-31 15:12:36 -0300

    editar e listar filmes

--- formacao_backend/bloco_14/projeto/src/controllers/movie-controller.js
+++ formacao_backend/bloco_14/projeto/src/controllers/movie-controller.js
@@ -1,5 +1,15 @@
 import { prisma } from "../helpers/utils.js";
 
+export const listAll = async (request, reply) => {
+  try {
+    const movies = await prisma.movie.findMany();
+    return movies;
+  } catch (error) {
+    console.log(error);
+    reply.status(500).send("Não foi possível listar filmes");
+  }
+};
+
 export const createMovie = async (request, reply) => {
   const { title, description, genderId, userId } = request.body;
 
@@ -16,5 +26,26 @@ export const createMovie = async (request, reply) => {
   } catch (error) {
     console.log(error);
     reply.status(400).send("Não foi possível criar o filme");
+  }
+};
+
+export const editMovie = async (request, reply) => {
+  const { movieId, title, description, genderId } = request.body;
+
+  try {
+    const movie = await prisma.movie.update({
+      where: {
+        id: movieId,
+      },
+      data: {
+        title,
+        description,
+        genderId,
+      },
+    });
+    return movie;
+  } catch (error) {
+    console.log(error);
+    reply.status(400).send("Não foi possível editar o filme");
   }
 };
--- formacao_backend/bloco_14/projeto/src/routes/movie-routes.js
+++ formacao_backend/bloco_14/projeto/src/routes/movie-routes.js
@@ -2,10 +2,21 @@ import { validateRequest } from "../middleware/auth.js";
 import * as movieController from "../controllers/movie-controller.js";
 
 export default {
+  listAllMovies: {
+    method: "GET",
+    url: "/movie",
+    handler: movieController.listAll,
+  },
   createMovie: {
     method: "POST",
     url: "/movie",
     preHandler: [validateRequest],
     handler: movieController.createMovie,
+  },
+  editMovie: {
+    method: "PATCH",
+    url: "/movie",
+    preHandler: [validateRequest],
+    handler: movieController.editMovie,
   },
 };
